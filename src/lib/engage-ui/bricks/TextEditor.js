7d4ed2e2e3c540bdc276c1f4663a780f3f371ad9
artyomgrishanov-qs <agrishanov@ambrahealth.com>
2020-03-05 11:20:23 +0300

    custom fonts in TextEditor

--- src/lib/engage-ui/bricks/TextEditor.js
+++ src/lib/engage-ui/bricks/TextEditor.js
@@ -3,6 +3,36 @@ import React from 'react'
 // https://github.com/zenoamaro/react-quill
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
+import '../style/rmx-text_editor.css';
+
+const
+    // To add a new font:
+    // 1. Add font into this array
+    // 2. Add classes in rmx-text_editor.css
+    fonts = ['Ubuntu', 'Inconsolata', 'Roboto', 'Mirza', 'Arial'],
+    importedFonts = {
+        // 'Ubuntu': true | false
+        // ...
+    };
+
+// https://github.com/zenoamaro/react-quill/issues/273
+const Font = ReactQuill.Quill.import('formats/font');
+Font.whitelist = [...fonts];
+ReactQuill.Quill.register(Font, true);
+
+function getStylesheetLink(family) {
+    return `https://fonts.googleapis.com/css?family=${family}`;
+}
+
+function addFont(family) {
+    if (!importedFonts[family]) {
+        const link = document.createElement('link');
+        link.href = getStylesheetLink(family);
+        link.rel = 'stylesheet';
+        document.getElementsByTagName('head')[0].append(link);
+        importedFonts[family] = true;
+    }
+}
 
 class TextEditor extends React.Component {
 
@@ -31,19 +61,27 @@ class TextEditor extends React.Component {
         else {
             this.setState({ stateText: value })
         }
+        // import font which is used in app
+        fonts.forEach( f => {
+            if (value.indexOf(`ql-font-${f}`) >= 0) {
+                addFont(f);
+            }
+        })
     }
 
     modules = {
         toolbar: [
-            ['bold', 'italic', 'underline'],                  // toggled buttons
-            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-            [{ 'font': [] }],
+            ['bold', 'italic', 'underline'],                   // toggled buttons
+            [{ 'size': ['small', false, 'large', 'huge' ] }],  // custom dropdown
+            [{ 'color': [] }, { 'background': [] }],           // dropdown with defaults from theme
+            [{ 'font': [ 'Ubuntu', 'Inconsolata', 'Roboto', 'Mirza', 'Arial' ] }],
             [{ 'align': [] }],
             ['link', 'clean']
         ]
     }
 
+    // 1. when you open editor first time in edit mode import all fonts
+    // 2. always - check which fonts is used and add styles dynamically
     render() {
         return (
             <div className={`rmx-text-editor ${this.props.readOnly ? '__readonly' : ''}`}>
@@ -56,9 +94,14 @@ class TextEditor extends React.Component {
     }
 
     componentDidMount() {
+
     }
 
     componentDidUpdate() {
+        if (!this.props.readOnly) {
+            // import all fonts in edit mode
+            fonts.forEach( f => addFont(f));
+        }
     }
 }
 
