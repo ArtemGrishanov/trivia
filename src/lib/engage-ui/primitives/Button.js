b87f9ae24a0d2f5f7ebc574832e10d560e011dc9
Dmitry <marutkin.dmitriy@gmail.com>
2020-04-10 11:00:05 +0300

    Dm arrow buttons improvements (#4)
    * Add pure arrows, svg way.
    
    * Resolve conflicts.
    
    * Add newline
    
    * Add arrowColor to component, demo examples added.
    
    * Typo fix

--- src/lib/engage-ui/primitives/Button.js
+++ src/lib/engage-ui/primitives/Button.js
@@ -1,47 +1,82 @@
 import React from 'react'
 import DataSchema from '../../schema'
 import RemixWrapper from '../RemixWrapper'
-import TextEditor from '../bricks/TextEditor'
+import TextEditor from '../bricks/TextEditor';
+import Arrow from '../bricks/Arrow';
 
 class Button extends React.Component {
+
     constructor(props) {
-        super(props)
-        this.state = {}
+        super(props);
+        this.state = {
+        }
     }
 
-    render() {
+    getMarkup(props) {
+
+        const {
+            id,
+            isArrow,
+            arrowType,
+            arrowDirection,
+            arrowPosition,
+            arrowColor,
+            doubleClicked,
+            dropShadow,
+            sizeMod,
+            text,
+        } = props;
+
         const st = {
             textAlign: 'initial',
             boxSizing: 'border-box',
             borderStyle: 'solid',
             ...Object.fromEntries(
-                ['borderRadius', 'borderWidth', 'borderColor', 'backgroundColor'].map(prop => {
-                    const value = this.props[prop]
+                ['borderRadius', 'borderWidth', 'borderColor', 'backgroundColor']
+                    .map(prop => {
+                        const value = this.props[prop]
 
-                    switch (typeof value) {
-                        case 'number':
-                            return [prop, `${value}px`]
-                        default:
-                            return [prop, value]
-                    }
-                }),
-            ),
+                        switch (typeof value) {
+                            case 'number':
+                                return [prop, `${value}px`];
+                            default:
+                                return [prop, value];
+                        }
+                    })
+            )
+        };
+
+        const arrowPositionOptions = {
+            right: '80%',
+            left: '20%',
+            center: '50%'
         }
-        if (this.props.dropShadow) {
-            st.boxShadow = '0px 2px 4px rgba(0, 0, 0, 0.5)'
+
+        const arrowSt = {
+            position: 'absolute',
+            top: '50%',
+            left: arrowPosition ? arrowPositionOptions[arrowPosition] : '50%',
+            transform: 'translate(-50%, -50%)',
+        };
+
+        if (dropShadow) {
+            st.boxShadow = '0px 2px 4px rgba(0, 0, 0, 0.5)';
         }
+
         return (
-            <button className={`rmx-component rmx-button  __${this.props.sizeMod}`} style={st}>
-                <div className="clipped">
-                    <TextEditor
-                        parentId={this.props.id}
-                        readOnly={!this.props.doubleClicked}
-                        text={this.props.text}
-                    ></TextEditor>
+            <button className={`rmx-component rmx-button  __${sizeMod}`} style={st}>
+                <div className='clipped'>
+                    {isArrow && <Arrow type={arrowType} direction={arrowDirection} st={arrowSt} color={arrowColor} />}
+                    <TextEditor parentId={id} readOnly={!doubleClicked} text={text} />
                 </div>
             </button>
         )
     }
+
+    render() {
+        return this.getMarkup(this.props);
+    }
+
 }
 
 /**
@@ -49,42 +84,65 @@ class Button extends React.Component {
  * Which props could be edited and how (types, range and other rules)
  */
 export const Schema = new DataSchema({
-    text: {
+    'text': {
         type: 'string',
         minLength: 1,
         maxLength: 4096,
-        default: 'Button text',
+        default: 'Button text'
     },
-    sizeMod: {
+    'sizeMod': {
         type: 'string',
         enum: ['small', 'normal'],
-        default: 'normal',
+        default: 'normal'
     },
-    borderRadius: {
+    'borderRadius': {
         type: 'number',
         min: 0,
         max: 100,
-        default: 0,
+        default: 0
     },
-    borderWidth: {
+    'borderWidth': {
         type: 'number',
         min: 0,
         max: 400,
-        default: 0,
+        default: 0
     },
-    borderColor: {
+    'borderColor': {
         type: 'string',
-        default: '',
+        default: ''
     },
-    dropShadow: {
+    'dropShadow': {
         type: 'boolean',
-        default: false,
+        default: false
     },
-    backgroundColor: {
+    'backgroundColor': {
         type: 'string',
-        default: 'blue',
+        default: 'blue'
     },
+    'isArrow': {
+        type: 'boolean',
+        default: false
+    },
+    'arrowType': {
+        type: 'string',
+        enum: ['triangle', 'thin', 'default'],
+        default: 'default'
+    },
+    'arrowDirection': {
+        type: 'string',
+        enum: ['left', 'right'],
+        default: 'right'
+    },
+    'arrowPosition': {
+        type: 'string',
+        enum: ['left', 'right', 'center'],
+        default: 'center'
+    },
+    'arrowColor': {
+        type: 'string',
+        default: '#fff'
+    }
     //TODO color format for strings, +tests
-})
+});
 
 export default RemixWrapper(Button, Schema, 'Button')
