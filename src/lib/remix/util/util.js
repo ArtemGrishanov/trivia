005effeb926a133e0579abedbec72c3c994c622b
Александр Янин <ayanin@MacBook-Air-Aleksandr.local>
2020-04-15 21:06:41 +0700

    Fixes the share button and debounce function

--- src/lib/remix/util/util.js
+++ src/lib/remix/util/util.js
@@ -106,15 +106,23 @@ export function flattenProperties(obj = {}, path = '', result = {}) {
 }
 
 export function debounce(func, wait, immediate) {
-    let timeout
+    let timeout = null
+
     return function () {
-        let context = this,
-            args = arguments
-        let later = function () {
+        const [context, args] = [this, arguments]
+        const later = function () {
             timeout = null
             if (!immediate) {
                 func.apply(context, args)
             }
+        }
+        const callNow = immediate && !timeout
+
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait)
+
+        if (callNow) {
+            func.apply(context, args)
         }
     }
 }
