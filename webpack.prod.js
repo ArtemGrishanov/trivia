7919d0912ca1f63846f1c7eadb80fb256f57210d
dmitriy.marutkin <marutkin.dmitriy@gmail.com>
2020-04-29 18:53:21 +0300

    Update prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,38 @@
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = env => {
     const { PROJECT_TYPE } = env || { PROJECT_TYPE: 'index' }
-    const buildMode = 'production'
     return merge(common(PROJECT_TYPE), {
-        mode: buildMode,
         devtool: false,
+        mode: 'none',
+        optimization: {
+            minimize: true,
+            minimizer: [new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    ecma: 5,
+                    warnings: false,
+                    parse: {},
+                    compress: {},
+                    mangle: {
+                        eval: false
+                    },
+                    module: false,
+                    toplevel: false,
+                    nameCache: null,
+                    keep_classnames: true,
+                    keep_fnames: true,
+                    safari10: true,
+                },
+            })],
+        },
+        plugins: [
+            new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+            new webpack.optimize.ModuleConcatenationPlugin(),
+            new webpack.NoEmitOnErrorsPlugin(),
+        ],
     })
 }
