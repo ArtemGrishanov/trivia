abc0fb025ad7ddb570ddd4149ea171626c6138fe
dmitriy.marutkin <marutkin.dmitriy@gmail.com>
2020-05-08 18:39:58 +0300

    Refactoring

--- src/lib/engage-ui/primitives/MemoryCard.js
+++ src/lib/engage-ui/primitives/MemoryCard.js
@@ -3,6 +3,20 @@ import React from 'react'
 
 const mainStyleClass = 'rmx-memory'
 
+function MemoryImage({ src, width, height, isInlineBlock }) {
+    const st = {
+        width: width || '100%',
+        height: height || '100%',
+        backgroundImage: `url(${src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: '50%',
+    }
+    if (isInlineBlock) {
+        st.display = 'inline-block'
+    }
+    return <div className={`${mainStyleClass}-image`} style={st}></div>
+}
+
 export default class MemoryCard extends React.Component {
     constructor(props) {
         super(props)
@@ -15,15 +29,15 @@ export default class MemoryCard extends React.Component {
     }
 
     render() {
-        const { width, relationKey } = this.props
+        const { src, width, padding, relationKey } = this.props
         const cardStyles = {
-            width: `${width}px`,
-            height: `${width}px`,
+            width: width,
+            padding: padding,
+            height: `100%`,
         }
         return (
             <div onClick={this.handleClick} style={cardStyles} className={`${mainStyleClass}-card`}>
-                {relationKey}
-                <ProgressiveImage left={0} top={0} {...this.props} />
+                <MemoryImage left={1} top={0} src={src} />
             </div>
         )
     }
--- src/lib/engage-ui/primitives/MemoryPlayground.js
+++ src/lib/engage-ui/primitives/MemoryPlayground.js
@@ -14,9 +14,10 @@ const DEBOUNCE_THRESHOLD = 650
 const CARD_BACK_IMG_URL = 'https://www.publicdomainpictures.net/pictures/40000/velka/question-mark.jpg'
 
 class CardData {
-    constructor(id, imgSrc) {
+    constructor(id, link, src) {
         this.id = id
-        this.imgSrc = imgSrc
+        this.link = link
+        this.src = src
     }
 }
 
@@ -24,112 +25,68 @@ class MemoryPlayground extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            cardWidth: 100,
-            cardHeight: 100,
             cardIndent: 5,
             prevSelectedCardId: null,
             cardsData: [],
         }
-        this.recalculateSizes = debounce(this.recalculateSizes.bind(this), DEBOUNCE_THRESHOLD)
         this.onCardClick = this.onCardClick.bind(this)
     }
 
     componentDidMount() {
         let cardsData = []
-        const { cardsCount } = this.props
-        if (cardsCount % 2 !== 0) {
+        const { rows, cardsPerRow } = this.props
+        if (rows % 2 !== 0) {
             return <div> Wrong number of cards was provided! </div>
         }
-        for (let i = 0; i < cardsCount; i++) {
-            cardsData.push(new CardData(i, CARD_BACK_IMG_URL))
+        let link = 0
+        for (let i = 0; i < rows; i++) {
+            const cards = []
+            for (let j = 0; j < cardsPerRow; j++) {
+                cards.push(new CardData(i, link, CARD_BACK_IMG_URL))
+            }
+            cardsData.push(cards)
         }
         this.setState({
             cardsData,
         })
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        this.recalculateSizes(this.props)
-    }
-
-    recalculateSizes({ width, height }) {
-        const { cardWidth, cardHeight } = this.state
-        const { cardsCount } = this.props
-        const playgroundArea = width * height
-        const fitCount = this.calcFigures({ width, height }, { width: cardWidth, height: cardHeight })
-
-        if (fitCount > cardsCount * 2) {
-            const cardArea = Math.floor(Math.sqrt(Math.floor(playgroundArea / cardsCount)))
-            this.setState(prevState => ({
-                cardWidth: cardArea,
-                cardHeight: cardArea,
-            }))
-        } else if (fitCount < cardsCount) {
-            this.setState(prevState => ({
-                cardWidth: prevState.cardWidth - 20,
-                cardHeight: prevState.cardHeight - 20,
-            }))
-        }
-    }
-
-    calcFigures(figureA, figureB) {
-        function calcTotalOne() {
-            let figures_per_row = Math.floor(figureA.width / figureB.width),
-                figures_per_col = Math.floor(figureA.height / figureB.height),
-                invers_figures_per_row = 0,
-                invers_figures_per_col = 0
-
-            if (figureA.width - figures_per_row * figureB.width >= figureB.height) {
-                invers_figures_per_row = Math.floor((figureA.width - figures_per_row * figureB.width) / figureB.height)
-                invers_figures_per_col = Math.floor(figureA.height / figureB.width)
-            }
-
-            return figures_per_row * figures_per_col + invers_figures_per_row * invers_figures_per_col
-        }
-
-        function calcTotalTwo() {
-            let figures_per_row = Math.floor(figureA.width / figureB.height),
-                figures_per_col = Math.floor(figureA.height / figureB.width),
-                invers_figures_per_row = 0,
-                invers_figures_per_col = 0
-
-            if (figureA.width - figures_per_row * figureB.height >= figureB.width) {
-                invers_figures_per_row = Math.floor((figureA.width - figures_per_row * figureB.height) / figureB.width)
-                invers_figures_per_col = Math.floor(figureA.height / figureB.height)
-            }
-
-            return figures_per_row * figures_per_col + invers_figures_per_row * invers_figures_per_col
-        }
-
-        return Math.max(calcTotalOne(), calcTotalTwo())
-    }
-
     onCardClick(card) {
         console.log(card)
-        const updatedCardsData = [...this.state.cardsData]
-        const index = updatedCardsData.findIndex(_card => _card.id === card.id)
-        updatedCardsData[index] = new CardData(card.id, 'http://p.testix.me/temp/cat1500x1000.jpg')
-        this.setState({
-            prevSelectedCardId: card.id,
-            cardsData: updatedCardsData,
-        })
+        // const updatedCardsData = [...this.state.cardsData]
+        // const index = updatedCardsData.findIndex(_card => _card.id === card.id)
+        // updatedCardsData[index] = new CardData(card.id, 'http://p.testix.me/temp/cat1500x1000.jpg')
+        // this.setState({
+        //     prevSelectedCardId: card.id,
+        //     cardsData: updatedCardsData,
+        // })
     }
 
     render() {
-        const { cardsData, cardWidth, cardHeight, cardIndent } = this.state
-        //.sort(() => Math.random() - 0.5)
+        const { cardsData, cardIndent } = this.state
+        const { rows, cardsPerRow } = this.props
+        const height = `${100 / rows}%`
+        const cardWidth = `${100 / cardsPerRow}%`
+        const indent = '5px'
+        const rowStyle = {
+            height: height,
+        }
+        console.log(cardsData, rowStyle)
         return (
             <div className={`${mainStyleClass}-playground`}>
-                {cardsData.map((card, i) => (
-                    <MemoryCard
-                        id={card.id}
-                        key={card.id}
-                        width={cardWidth}
-                        height={cardHeight}
-                        margin={cardIndent}
-                        src={card.imgSrc}
-                        clickHandler={this.onCardClick}
-                    />
+                {cardsData.map((row, i) => (
+                    <div style={rowStyle} className={`${mainStyleClass}-row`} key={i}>
+                        {row.map(card => (
+                            <MemoryCard
+                                padding={indent}
+                                height={height}
+                                width={cardWidth}
+                                key={Math.random()}
+                                src={card.src}
+                                clickHandler={this.onCardClick}
+                            />
+                        ))}
+                    </div>
                 ))}
             </div>
         )
@@ -141,11 +98,16 @@ class MemoryPlayground extends React.Component {
  * Which props could be edited and how (types, range and other rules)
  */
 export const Schema = new DataSchema({
-    cardsCount: {
+    rows: {
         type: 'number',
         min: 4,
-        max: 32,
-        enum: [4, 8, 16, 32],
+        max: 6,
+        default: 4,
+    },
+    cardsPerRow: {
+        type: 'number',
+        min: 2,
+        max: 6,
         default: 4,
     },
 })
