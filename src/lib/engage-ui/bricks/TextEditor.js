90659726ad2b2a4ed491426ff9644b6c7de6c01e
Александр Янин <ayanin@MacBook-Air-Aleksandr.local>
2020-05-27 13:25:51 +0700

    Fixes resetting TextEditor fonts after switching screen

--- src/lib/engage-ui/bricks/TextEditor.js
+++ src/lib/engage-ui/bricks/TextEditor.js
@@ -8,7 +8,7 @@ import '../style/rmx-text_editor.css'
 
 const // To add a new font:
     // 1. Add font into this array
-    // 2. Add classes in rmx-text_editor.css
+    // 2. Add classes as UpperCamelCase in rmx-text_editor.css
     fonts = [
         'Roboto',
         'Roboto Condensed',
@@ -31,7 +31,7 @@ const // To add a new font:
         'Yeseva One',
         'Marmelad',
         'Rubik Mono One',
-    ].map(f => f.split(' ').join('-')),
+    ],
     importedFonts = {
         // 'Ubuntu': true | false
         // ...
@@ -39,11 +39,11 @@ const // To add a new font:
 
 // https://github.com/zenoamaro/react-quill/issues/273
 const Font = ReactQuill.Quill.import('formats/font')
-Font.whitelist = [...fonts]
+Font.whitelist = fonts.map(f => f.replace(/ /g, ''))
 ReactQuill.Quill.register(Font, true)
 
 function getStylesheetLink(family) {
-    return `https://fonts.googleapis.com/css?family=${family.split('-').join(' ')}`
+    return `https://fonts.googleapis.com/css?family=${family}`
 }
 
 function addFont(family) {
@@ -57,29 +57,20 @@ function addFont(family) {
 }
 
 class TextEditor extends React.Component {
-    static getDerivedStateFromProps(props, state) {
-        return {
-            ...state,
-            prevPropText: props.text,
-            stateText: state.prevPropText !== props.text ? props.text : state.stateText,
-        }
-    }
-
     constructor(props) {
         super(props)
         this.state = {
-            stateText: props.text,
-            prevPropText: props.text,
+            html: props.text,
         }
         this.handleChange = this.handleChange.bind(this)
         this.contentRef = React.createRef()
     }
 
-    handleChange(value) {
-        this.setState({ stateText: value })
+    handleChange(html) {
+        this.setState({ html })
         // import font which is used in app
-        fonts.forEach(f => {
-            if (value.indexOf(`ql-font-${f}`) >= 0) {
+        fonts.forEach((f, i) => {
+            if (html.includes(`ql-font-${Font.whitelist[i]}`)) {
                 addFont(f)
             }
         })
@@ -90,7 +81,7 @@ class TextEditor extends React.Component {
             ['bold', 'italic', 'underline'], // toggled buttons
             [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
             [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-            [{ font: fonts }],
+            [{ font: Font.whitelist }],
             [{ align: [] }],
             ['link', 'clean'],
         ],
@@ -99,12 +90,13 @@ class TextEditor extends React.Component {
     // 1. when you open editor first time in edit mode import all fonts
     // 2. always - check which fonts is used and add styles dynamically
     render() {
+        const { readOnly } = this.props
         return (
-            <div className={`rmx-text-editor ${this.props.readOnly ? '__readonly' : ''}`} ref={this.contentRef}>
+            <div className={`rmx-text-editor ${readOnly ? '__readonly' : ''}`} ref={this.contentRef}>
                 <ReactQuill
-                    readOnly={this.props.readOnly}
+                    readOnly={readOnly}
                     modules={this.modules}
-                    value={this.state.stateText}
+                    value={this.state.html}
                     onChange={this.handleChange}
                 />
             </div>
@@ -112,21 +104,21 @@ class TextEditor extends React.Component {
     }
 
     componentDidMount() {
-        if (this.props.getContentRect) {
-            if (this.contentRef && this.contentRef.current) {
-                this.props.getContentRect(this.contentRef.current.getBoundingClientRect())
-            }
+        const { getContentRect } = this.props
+        if (getContentRect && this.contentRef && this.contentRef?.current) {
+            getContentRect(this.contentRef.current.getBoundingClientRect())
         }
     }
 
-    componentDidUpdate(prevProps) {
-        if (!this.props.readOnly) {
+    componentDidUpdate({ readOnly: prevReadOnly }) {
+        const { readOnly, parentId } = this.props
+        if (!readOnly) {
             // import all fonts in edit mode
             fonts.forEach(f => addFont(f))
         }
-        if (!prevProps.readOnly && this.props.readOnly) {
+        if (!prevReadOnly && readOnly) {
             // выход из режима ввода текста - делаем сохранение в remix
-            setComponentProps(this.props.parentId, { text: this.state.stateText }, { putStateHistory: true })
+            setComponentProps(parentId, { text: this.state.html }, { putStateHistory: true })
         }
     }
 }
