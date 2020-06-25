e7a934ba40af9641335132ec721f2022b7e35a33
dmitry_kuzmenko_qs <dmitry.kuzmenko@quantumsoft.ru>
2020-06-25 21:27:40 +0700

    refactoring

--- src/lib/engage-ui/UserDataForm.js
+++ src/lib/engage-ui/UserDataForm.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { setComponentProps } from '../remix'
 import DataSchema from '../schema'
 import RemixWrapper from './RemixWrapper'
-import { Input } from '../engage-ui/primitives/Input'
-import { Button } from '../engage-ui/primitives/Button'
-import { TextEditor } from '../engage-ui/bricks/TextEditor'
+import { Input } from './primitives/Input'
+import { Button } from './primitives/Button'
+import { TextEditor } from './bricks/TextEditor'
+// import BasicImage from './bricks/BasicImage'
 
-import './style/user-data-form.css'
+import './style/rmx-user-data-form.css'
 
-const UserDataForm = ({ id, doubleClicked, firstNameField, lastNameField, phoneField, emailField, needTitle }) => {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [phone, setPhone] = useState('')
-    const [licenseAgreement, setLicenseAgreement] = useState(false)
+class UserDataForm extends React.Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        licenseAgreement: false,
+    }
 
-    const setData = () => {
+    setFirstName = value => this.setState({ ...this.state, firstName: value })
+
+    setLastName = value => this.setState({ ...this.state, lastName: value })
+
+    setEmail = value => this.setState({ ...this.state, email: value })
+
+    setPhone = value => this.setState({ ...this.state, phone: value })
+
+    setLicenseAgreement = value => this.setState({ ...this.state, licenseAgreement: value })
+
+    setData = () => {
+        const { id } = this.props
+        const { firstName, lastName, email, phone } = this.state
+
         setComponentProps({
             id,
             data: {
@@ -27,7 +43,10 @@ const UserDataForm = ({ id, doubleClicked, firstNameField, lastNameField, phoneF
         })
     }
 
-    const checkFormCompletion = () => {
+    checkFormCompletion = () => {
+        const { firstNameField, lastNameField, phoneField, emailField } = this.props
+        const { firstName, lastName, email, phone, licenseAgreement } = this.state
+
         const isFirstName = firstNameField ? firstName !== '' : true
         const isLastName = lastNameField ? lastName !== '' : true
         const isPhone = phoneField ? phone !== '' : true
@@ -36,92 +55,111 @@ const UserDataForm = ({ id, doubleClicked, firstNameField, lastNameField, phoneF
         return licenseAgreement && isFirstName && isLastName && isPhone && isEmail
     }
 
-    return (
-        <div className="user-data-form clipped">
-            <div className="user-data-form__content">
-                {needTitle ? (
-                    <div className="user-data-form__title">
-                        <TextEditor
-                            parentId={id}
-                            readOnly={!doubleClicked}
-                            text={`<span class="ql-size-huge ql-font-Roboto" style="color: #000000">Your title</span>`}
-                        />
-                    </div>
-                ) : null}
-                {firstNameField ? (
-                    <div className="user-data-form__text-input">
-                        <Input
-                            description="First name"
-                            dataType="any"
-                            dataSize={128}
-                            width={252}
-                            height={83}
-                            setValue={setFirstName}
-                        />
-                    </div>
-                ) : null}
-                {lastNameField ? (
-                    <div className="user-data-form__text-input">
-                        <Input
-                            description="Last name"
-                            dataType="any"
-                            dataSize={128}
-                            width={252}
-                            height={83}
-                            setValue={setLastName}
-                        />
-                    </div>
-                ) : null}
-                {phoneField ? (
-                    <div className="user-data-form__text-input">
-                        <Input
-                            description="Phone"
-                            dataType="phone"
-                            dataSize={32}
-                            width={252}
-                            height={83}
-                            setValue={setPhone}
-                        />
-                    </div>
-                ) : null}
-                {emailField ? (
-                    <div className="user-data-form__text-input">
-                        <Input
-                            description="Email"
-                            dataType="email"
-                            dataSize={64}
-                            width={252}
-                            height={83}
-                            setValue={setEmail}
-                        />
-                    </div>
-                ) : null}
-                <div className="user-data-form__submit" onClick={checkFormCompletion() ? setData : void 0}>
-                    <Button
-                        width={252}
-                        height={44}
-                        text={`<p class="ql-align-center"><span class="ql-size-large ql-font-Roboto" style="color: #FFFFFF">Next</span></p>`}
-                        imageSrc={''}
+    render() {
+        const { id, doubleClicked, firstNameField, lastNameField, phoneField, emailField, needTitle } = this.props
+
+        return (
+            <div className="user-data-form clipped">
+                <div className="user-data-form__bg">
+                    {/* <BasicImage
+                        width={this.props.width}
+                        height={this.props.height}
+                        backgroundSize={'cover'}
+                        blur={false}
+                        grayscale={false}
                         borderRadius={4}
+                        borderColor="#"
                         borderWidth={0}
-                        borderColor=""
-                        backgroundColor={checkFormCompletion() ? '#2990FB' : '#BCBCBC'}
-                        styleVariant="none"
-                        sizeMod="normal"
-                        // doubleClicked={doubleClicked}
-                        id={`${id}_submitButton`}
-                    />
+                        backgroundColor="#ffffff"
+                    ></BasicImage> */}
                 </div>
-                <div className="user-data-form__agree">
-                    <input type="checkbox" onChange={event => setLicenseAgreement(event.target.checked)} />
-                    <span>
-                        Agree with{' '}
-                        <a onClick={() => window.open('https://interacty.me/privacy-policy')}>Privacy Policy</a>
-                    </span>
+                <div className="user-data-form__content">
+                    {needTitle ? (
+                        <div className="user-data-form__title">
+                            <TextEditor
+                                parentId={id}
+                                readOnly={!doubleClicked}
+                                text={`<span class="ql-size-huge ql-font-Roboto" style="color: #000000">Your title</span>`}
+                            />
+                        </div>
+                    ) : null}
+                    {firstNameField ? (
+                        <div className="user-data-form__text-input">
+                            <Input
+                                description="First name"
+                                dataType="any"
+                                dataSize={128}
+                                width={252}
+                                height={83}
+                                setValue={this.setFirstName}
+                            />
+                        </div>
+                    ) : null}
+                    {lastNameField ? (
+                        <div className="user-data-form__text-input">
+                            <Input
+                                description="Last name"
+                                dataType="any"
+                                dataSize={128}
+                                width={252}
+                                height={83}
+                                setValue={this.setLastName}
+                            />
+                        </div>
+                    ) : null}
+                    {phoneField ? (
+                        <div className="user-data-form__text-input">
+                            <Input
+                                description="Phone"
+                                dataType="phone"
+                                dataSize={32}
+                                width={252}
+                                height={83}
+                                setValue={this.setPhone}
+                            />
+                        </div>
+                    ) : null}
+                    {emailField ? (
+                        <div className="user-data-form__text-input">
+                            <Input
+                                description="Email"
+                                dataType="email"
+                                dataSize={64}
+                                width={252}
+                                height={83}
+                                setValue={this.setEmail}
+                            />
+                        </div>
+                    ) : null}
+                    <div
+                        className="user-data-form__submit"
+                        onClick={this.checkFormCompletion() ? this.setData : void 0}
+                    >
+                        <Button
+                            width={252}
+                            height={44}
+                            text={`<p class="ql-align-center"><span class="ql-size-large ql-font-Roboto" style="color: #FFFFFF">Next</span></p>`}
+                            imageSrc={''}
+                            borderRadius={4}
+                            borderWidth={0}
+                            borderColor=""
+                            backgroundColor={this.checkFormCompletion() ? '#2990FB' : '#BCBCBC'}
+                            styleVariant="none"
+                            sizeMod="normal"
+                            id={`${id}_submitButton`}
+                        />
+                    </div>
+                    <div className="user-data-form__agree">
+                        <input type="checkbox" onChange={event => this.setLicenseAgreement(event.target.checked)} />
+                        <span>
+                            Agree with{' '}
+                            <a onClick={() => window.open('https://interacty.me/privacy-policy')}>Privacy Policy</a>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 const schema = {
--- src/lib/engage-ui/primitives/Input.js
+++ src/lib/engage-ui/primitives/Input.js
@@ -1,95 +1,176 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { setComponentProps } from '../../remix'
 import DataSchema from '../../schema'
 import RemixWrapper from '../RemixWrapper'
 
 import '../style/rmx-input.css'
 
-const validator = (type, value) => {
-    const helpData = {
-        email: {
-            pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
-            errMsg: 'Please enter a valid email address',
-        },
-        phone: {
-            pattern: /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm,
-            errMsg: 'Please enter a valid phone number',
-        },
-    }
+class Input extends React.Component {
+    static validator = (type, value) => {
+        const helpData = {
+            email: {
+                pattern: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+                errMsg: 'Please enter a valid email address',
+            },
+            phone: {
+                pattern: /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm,
+                errMsg: 'Please enter a valid phone number',
+            },
+        }
 
-    if (!(type in helpData)) {
-        return { isError: false }
-    }
+        if (!(type in helpData)) {
+            return { isError: false }
+        }
 
-    const result = value.match(helpData[type].pattern)
+        const result = value.match(helpData[type].pattern)
 
-    if (result === null || result.join('') !== value) {
-        return { isError: true, msg: helpData[type].errMsg }
-    } else {
-        return { isError: false }
-    }
-}
-
-const Input = ({ id, doubleClicked, dataType, dataSize, description, width, height, setValue }) => {
-    if (setValue === void 0) {
-        setValue = value => {
-            setComponentProps(
-                {
-                    id,
-                    value,
-                },
-                { putStateHistory: true },
-            )
+        if (result === null || result.join('') !== value) {
+            return { isError: true, msg: helpData[type].errMsg }
+        } else {
+            return { isError: false }
         }
     }
-    const [focused, setFocus] = useState(false)
-    const [result, setResult] = useState({ isError: false, msg: '' })
 
-    const descriptionStyle = {
-        color: '#787878',
-        fontSize: '14px',
-        marginBottom: '6px',
+    state = {
+        focused: false,
+        result: { isError: false, msg: '' },
     }
 
-    const inputStyle = {
-        borderStyle: 'solid',
-        backgroundColor: 'transparent',
-        borderRadius: '4px',
-        fontSize: '16px',
-        fontColor: '#3c3c3c',
-        fontFamily: 'Ubuntu',
-        ...(focused
-            ? { borderWidth: '2px', borderColor: '#69b1fc', caretColor: '#69b1fc' }
-            : result.isError
-            ? { borderWidth: '1px', borderColor: '#FF5656', caretColor: '#FF5656' }
-            : { borderWidth: '1px', borderColor: '#979797', caretColor: '#979797' }),
+    setFocus = value => this.setState({ ...this.state, focused: value })
+
+    setResult = value => this.setState({ ...this.state, result: value })
+
+    setValueDefault = value => {
+        const { id } = this.props
+
+        setComponentProps(
+            {
+                id,
+                value,
+            },
+            { putStateHistory: true },
+        )
     }
 
-    return (
-        <div className="remix-input" style={{ width: `${width}px`, height: `${height}px` }}>
-            {description.length ? <span style={descriptionStyle}>{description}</span> : null}
-            <input
-                type="text"
-                style={inputStyle}
-                onFocus={() => setFocus(true)}
-                onBlur={event => {
-                    setFocus(false)
-                }}
-                onChange={event => {
-                    const result = validator(dataType, event.target.value)
-                    setResult(result)
+    render() {
+        const {
+            doubleClicked,
+            dataType,
+            dataSize,
+            description,
+            width,
+            height,
+            setValue = this.setValueDefault,
+        } = this.props
+        const { focused, result } = this.state
 
-                    if (!result.isError) {
-                        setValue(event.target.value)
-                    }
-                }}
-                max={dataSize}
-                readOnly={doubleClicked}
-            />
-            <p className="remix-input__msg">{result.isError ? result.msg : ''}</p>
-        </div>
-    )
+        const descriptionStyle = {
+            color: '#787878',
+            fontSize: '14px',
+            marginBottom: '6px',
+        }
+
+        const inputStyle = {
+            borderStyle: 'solid',
+            backgroundColor: 'transparent',
+            borderRadius: '4px',
+            fontSize: '16px',
+            fontColor: '#3c3c3c',
+            fontFamily: 'Ubuntu',
+            ...(focused
+                ? { borderWidth: '2px', borderColor: '#69b1fc', caretColor: '#69b1fc' }
+                : result.isError
+                ? { borderWidth: '1px', borderColor: '#FF5656', caretColor: '#FF5656' }
+                : { borderWidth: '1px', borderColor: '#979797', caretColor: '#979797' }),
+        }
+
+        return (
+            <div className="remix-input" style={{ width: `${width}px`, height: `${height}px` }}>
+                {description.length ? <span style={descriptionStyle}>{description}</span> : null}
+                <input
+                    type="text"
+                    style={inputStyle}
+                    onFocus={() => this.setFocus(true)}
+                    onBlur={event => {
+                        this.setFocus(false)
+                    }}
+                    onChange={event => {
+                        const result = Input.validator(dataType, event.target.value)
+                        this.setResult(result)
+
+                        if (!result.isError) {
+                            setValue(event.target.value)
+                        }
+                    }}
+                    max={dataSize}
+                    readOnly={doubleClicked}
+                />
+                <p className="remix-input__msg">{result.isError ? result.msg : ''}</p>
+            </div>
+        )
+    }
 }
+
+// const Input = ({ id, doubleClicked, dataType, dataSize, description, width, height, setValue }) => {
+//     if (setValue === void 0) {
+//         setValue = value => {
+//             setComponentProps(
+//                 {
+//                     id,
+//                     value,
+//                 },
+//                 { putStateHistory: true },
+//             )
+//         }
+//     }
+//     const [focused, setFocus] = useState(false)
+//     const [result, setResult] = useState({ isError: false, msg: '' })
+
+//     const descriptionStyle = {
+//         color: '#787878',
+//         fontSize: '14px',
+//         marginBottom: '6px',
+//     }
+
+//     const inputStyle = {
+//         borderStyle: 'solid',
+//         backgroundColor: 'transparent',
+//         borderRadius: '4px',
+//         fontSize: '16px',
+//         fontColor: '#3c3c3c',
+//         fontFamily: 'Ubuntu',
+//         ...(focused
+//             ? { borderWidth: '2px', borderColor: '#69b1fc', caretColor: '#69b1fc' }
+//             : result.isError
+//             ? { borderWidth: '1px', borderColor: '#FF5656', caretColor: '#FF5656' }
+//             : { borderWidth: '1px', borderColor: '#979797', caretColor: '#979797' }),
+//     }
+
+//     return (
+//         <div className="remix-input" style={{ width: `${width}px`, height: `${height}px` }}>
+//             {description.length ? <span style={descriptionStyle}>{description}</span> : null}
+//             <input
+//                 type="text"
+//                 style={inputStyle}
+//                 onFocus={() => setFocus(true)}
+//                 onBlur={event => {
+//                     setFocus(false)
+//                 }}
+//                 onChange={event => {
+//                     const result = validator(dataType, event.target.value)
+//                     setResult(result)
+
+//                     if (!result.isError) {
+//                         setValue(event.target.value)
+//                     }
+//                 }}
+//                 max={dataSize}
+//                 readOnly={doubleClicked}
+//             />
+//             <p className="remix-input__msg">{result.isError ? result.msg : ''}</p>
+//         </div>
+//     )
+// }
 
 export const schema = {
     dataType: {
