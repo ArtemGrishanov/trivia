af4e5a8dabea637c63211ee5eb345fa03f02e4f4
dmitry_kuzmenko_qs <dmitry.kuzmenko@quantumsoft.ru>
2020-06-25 22:38:49 +0700

    refactoring

--- src/lib/engage-ui/primitives/Input.js
+++ src/lib/engage-ui/primitives/Input.js
@@ -111,67 +111,6 @@ class Input extends React.Component {
     }
 }
 
-// const Input = ({ id, doubleClicked, dataType, dataSize, description, width, height, setValue }) => {
-//     if (setValue === void 0) {
-//         setValue = value => {
-//             setComponentProps(
-//                 {
-//                     id,
-//                     value,
-//                 },
-//                 { putStateHistory: true },
-//             )
-//         }
-//     }
-//     const [focused, setFocus] = useState(false)
-//     const [result, setResult] = useState({ isError: false, msg: '' })
-
-//     const descriptionStyle = {
-//         color: '#787878',
-//         fontSize: '14px',
-//         marginBottom: '6px',
-//     }
-
-//     const inputStyle = {
-//         borderStyle: 'solid',
-//         backgroundColor: 'transparent',
-//         borderRadius: '4px',
-//         fontSize: '16px',
-//         fontColor: '#3c3c3c',
-//         fontFamily: 'Ubuntu',
-//         ...(focused
-//             ? { borderWidth: '2px', borderColor: '#69b1fc', caretColor: '#69b1fc' }
-//             : result.isError
-//             ? { borderWidth: '1px', borderColor: '#FF5656', caretColor: '#FF5656' }
-//             : { borderWidth: '1px', borderColor: '#979797', caretColor: '#979797' }),
-//     }
-
-//     return (
-//         <div className="remix-input" style={{ width: `${width}px`, height: `${height}px` }}>
-//             {description.length ? <span style={descriptionStyle}>{description}</span> : null}
-//             <input
-//                 type="text"
-//                 style={inputStyle}
-//                 onFocus={() => setFocus(true)}
-//                 onBlur={event => {
-//                     setFocus(false)
-//                 }}
-//                 onChange={event => {
-//                     const result = validator(dataType, event.target.value)
-//                     setResult(result)
-
-//                     if (!result.isError) {
-//                         setValue(event.target.value)
-//                     }
-//                 }}
-//                 max={dataSize}
-//                 readOnly={doubleClicked}
-//             />
-//             <p className="remix-input__msg">{result.isError ? result.msg : ''}</p>
-//         </div>
-//     )
-// }
-
 export const schema = {
     dataType: {
         type: 'string',
