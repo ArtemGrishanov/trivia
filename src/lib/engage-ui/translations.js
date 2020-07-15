1404fa247c61093d1814993521dafc8c991dfac6
dmitry_kuzmenko_qs <dmitry.kuzmenko@quantumsoft.ru>
2020-07-15 12:43:45 +0700

    fix translations

--- src/lib/engage-ui/translations.js
+++ src/lib/engage-ui/translations.js
@@ -29,11 +29,11 @@ const translations = {
     },
     invalid_email: {
         en: 'Please enter a valid email address',
-        ru: 'Введите валидный адрес эл. почты',
+        ru: 'Введите корректный адрес эл. почты',
     },
     invalid_phone: {
         en: 'Please enter a valid phone number',
-        ru: 'Введите валидный телефонный номер',
+        ru: 'Введите корректный телефонный номер',
     },
 }
 
