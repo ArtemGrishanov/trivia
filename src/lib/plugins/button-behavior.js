6be64133273d78244c5cb1541ecc22c1d1bcc7b5
dmitry_kuzmenko_qs <dmitry.kuzmenko@quantumsoft.ru>
2020-08-10 17:45:59 +0700

    vk share

--- src/lib/plugins/button-behavior.js
+++ src/lib/plugins/button-behavior.js
@@ -1,8 +1,14 @@
-const FACEBOOK_BUTTON = 'FACEBOOK_BUTTON'
-const FACEBOOK_TAG = 'facebook'
+import { Selector } from '../object-path'
+import { getComponentIdFromPath, getScreenIdFromPath } from '../../lib/remix/util/util'
+
+const INIT = 'INIT'
+const FACEBOOK_SHARE = 'FACEBOOK_SHARE'
+const FACEBOOK_SHARE_TAG = 'facebook'
+const VK_SHARE = 'VK_SHARE'
+const VK_SHARE_TAG = 'vkontakte'
 
 const initButtonBehavior = ({ remix }) => {
-    remix.registerTriggerAction(FACEBOOK_BUTTON, event => {
+    remix.registerTriggerAction(FACEBOOK_SHARE, event => {
         try {
             const state = event.remix.getState(),
                 id = event.eventData.id,
@@ -28,9 +34,38 @@ const initButtonBehavior = ({ remix }) => {
     remix.addTrigger({
         when: {
             eventType: 'onclick',
-            condition: { prop: 'tags', clause: 'CONTAINS', value: FACEBOOK_TAG },
+            condition: { prop: 'tags', clause: 'CONTAINS', value: FACEBOOK_SHARE_TAG },
         },
-        then: { actionType: FACEBOOK_BUTTON },
+        then: { actionType: FACEBOOK_SHARE },
+    })
+
+    remix.registerTriggerAction(VK_SHARE, event => {
+        try {
+            const state = event.remix.getState(),
+                id = event.eventData.id,
+                entity = state.app.share.entities.toArray().find(e => e.componentId === id)
+
+            if (entity && entity.href) {
+                const title = entity.title ? `&title=${entity.title}` : ''
+                const imageUrl = entity.imageUrl ? `&image=${entity.imageUrl}` : ''
+
+                window.open(`https://vk.com/share.php?url=${entity.href}${title}${imageUrl}`)
+
+                Remix.fireEvent('vk_share_requested')
+            } else {
+                console.error('Share entity not found or href not set')
+            }
+        } catch (err) {
+            console.error('Share entities not configured properly', err.message)
+        }
+    })
+
+    remix.addTrigger({
+        when: {
+            eventType: 'onclick',
+            condition: { prop: 'tags', clause: 'CONTAINS', value: VK_SHARE_TAG },
+        },
+        then: { actionType: VK_SHARE },
     })
 }
 
