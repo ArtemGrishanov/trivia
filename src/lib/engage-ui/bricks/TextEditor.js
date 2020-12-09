1250d75e2befdcea8c4391f5b1110fd65e1f843e
proaleks94 <aleksey.pronin@quantumsoft.ru>
2020-12-09 19:19:42 +0700

    update quill

--- src/lib/engage-ui/bricks/TextEditor.js
+++ src/lib/engage-ui/bricks/TextEditor.js
@@ -10,7 +10,7 @@ const Parchment = ReactQuill.Quill.import('parchment')
 // line-height
 const lineHeightConfig = {
     scope: Parchment.Scope.INLINE,
-    whitelist: ['1.0', '1.2', '1.5', '1.6', '1.8', '2.0', '2.4', '2.8', '3.0', '4.0', '5.0'],
+    whitelist: ['1.0', '1.2', '1.5', '1.6', '1.8', '2.0', '2.4', '2.8'],
 }
 const lineHeightClass = new Parchment.Attributor.Class('lineheight', 'ql-line-height', lineHeightConfig)
 const lineHeightStyle = new Parchment.Attributor.Style('lineheight', 'line-height', lineHeightConfig)
@@ -26,7 +26,9 @@ const fontSizeConfig = {
         '11px',
         '12px',
         '14px',
+        '16px',
         '18px',
+        '20px',
         '24px',
         '30px',
         '36px',
